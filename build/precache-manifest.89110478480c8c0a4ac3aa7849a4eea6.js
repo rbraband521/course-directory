89d1c3f21bc90cb16f5f4c94558182dbf1273f85
rbraband521 <57102562+rbraband521@users.noreply.github.com>
2020-07-16 11:17:54 -0500

    Add files via upload

---
+++ build/precache-manifest.89110478480c8c0a4ac3aa7849a4eea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d22f378c301b999becc",
+    "url": "/static/css/main.daa5a50f.chunk.css"
+  },
+  {
+    "revision": "9d22f378c301b999becc",
+    "url": "/static/js/main.9d22f378.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e869ea830f0c42a99af5b0dc28e1dc7",
+    "url": "/index.html"
+  }
+];
